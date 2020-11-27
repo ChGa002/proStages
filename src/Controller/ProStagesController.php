1d3973b828261b66974d3d2a7ffcda652c66b4d7
Chiara Gandolfi <cgandolfi@iutbayonne.univ-pau.fr>
2020-11-27 21:58:15 +0100

    Mise en place des 4 urls finales avec des réponses brutes

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -11,10 +11,44 @@ class ProStagesController extends AbstractController
     /**
      * @Route("/", name="prostages_accueil")
      */
+
     public function index(): Response
     {
-        return $this->render('pro_stages/index.html.twig', [
+        /*return $this->render('pro_stages/index.html.twig', [
             'controller_name' => 'ProStagesController',
-        ]);
+        ]); */
+
+        return new Response ('<h1> Bienvenue sur la page d\'accueil de Prostages </h1 >');
     }
+
+    /**
+     * @Route("/entreprises", name="prostages_entreprises")
+     */
+    
+    public function afficherEntreprises(): Response
+    {
+
+        return new Response ('<h1> Cette page affichera la liste des entreprises proposant un stage </h1 >');
+    }
+
+    /**
+     * @Route("/formations", name="prostages_formations")
+     */
+    
+    public function afficherFormations(): Response
+    {
+
+        return new Response ('<h1> Cette page affichera la liste des formations de l\'IUT </h1 >');
+    }
+
+    /**
+     * @Route("/stages/{id}", name="prostages_stages")
+     */
+    
+    public function afficherStage($id): Response
+    {
+
+        return new Response("Cette page affichera le descriptif du stage ayant pour identifiant $id");
+    }
+
 }
