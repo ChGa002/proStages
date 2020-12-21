891d60ed9a26f25d6e81f510b0602a77ab1ca5be
Chiara Gandolfi <cgandolfi@iutbayonne.univ-pau.fr>
2020-12-21 21:25:50 +0100

    Creation et generation complete d'un jeu de donnees

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,27 +13,27 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
     	// Creation d'un faker francais
-    	$faker = Faker\Factory::create('fr_FR'); 
+    	$faker = \Faker\Factory::create('fr_FR'); 
 
 
         /***************************************
         *** CREATION DES FORMATIONS ***
         ****************************************/
-        $DUTINFO = new Formation();
-        $DUTINFO->setFormation("DUT Informatique");
+        $dutInfo = new Formation();
+        $dutInfo->setFormation("DUT Informatique");
 
-        $DUTGIM = new Formation();
-        $DUTGIM->setFormation("DUT Génie industriel et maintenance");
+        $dutGim = new Formation();
+        $dutGim->setFormation("DUT Génie industriel et maintenance");
 
-        $LPNUM = new Formation();
-        $LPNUM->setFormation("LP Métiers du Numérique");
+        $lpNum = new Formation();
+        $lpNum->setFormation("LP Métiers du Numérique");
 
-        $LPPA = new Formation();
-        $LPPA->setFormation("LP Programmation avancée");
+        $lpPa = new Formation();
+        $lpPa->setFormation("LP Programmation avancée");
 
         /* On regroupe les objets "formation" dans un tableau
         pour pouvoir s'y référer au moment de la création d'un stage particulier */
-        $tableauFormations = array($DUTINFO,$DUTGIM,$LPNUM,$LPPA);
+        $tableauFormations = array($dutInfo,$dutGim,$lpNum,$lpPa);
 
         // Mise en persistance des objets formation
         foreach ($tableauFormations as $formation) {
@@ -44,67 +44,62 @@ class AppFixtures extends Fixture
         *** CREATION DES ENTREPRISES ***
         ****************************************/
 
-         $nbEntreprises = 15;
+        $nbEntreprises = 9;
         for ($i=1; $i <= $nbEntreprises; $i++) {
+
             // Création d'un nouveau stage
-        $Entreprise = new Entreprise();
-        $Entreprise->setNom($faker->company) 
-        $manager->flush();
+	        $entreprise = new Entreprise();
+	        $entreprise->setNom($faker->company);
+	        $entreprise->setActivite($faker->jobTitle);
+			$entreprise->setAdresse($faker->address);
 
+	        $manager->persist($entreprise);
 
-        /********************************************************
-        *** CREATION DES MODULES ET DES RESSOURCES ASSOCIEES  ***
-        *********************************************************/
-        foreach ($modulesDutInfo as $codeModule => $titreModule) {
-            // ************* Création d'un nouveau module *************
-            $module = new Module();
-            // Génération d'un numéro de semestre compris entre 1 et 4
-            $numSemestre = $faker->numberBetween($min = 1, $max = 4);
-            // Définition du code du semestre
-            $module->setCode($faker->regexify('M'.$numSemestre.'[1-2]0[1-6]'));
-            $module->setCode($codeModule);
-            // Définition du titre du semestre
-            $module->setTitre($faker->sentence($nbWords = 6, $variableNbWords = true));
-            $module->setTitre($titreModule);
-            // Définition du numéro du semestre
-            $module->setSemestre($numSemestre);
-            $module->setSemestre($codeModule[1]);
-            // Enregistrement du module créé
-            $manager->persist($module);
+	        $nbStages = $faker->numberBetween($min = 1, $max = 5);
+	        /********************************************************
+	        *** CREATION DES STAGES D'UNE ENTREPRISE  ***
+	        *********************************************************/
+	      	for ($j=1 ; $j <= $nbStages; $j++)
+	      	{
+	            // ************* Création d'un nouveau stage *************
+	            $stage = new Stage();
 
-            // **** Création de plusieurs ressources associées au module
-            $nbRessourcesAGenerer = $faker->numberBetween($min = 0, $max = 7);
-            for ($numRessource=0; $numRessource < $nbRessourcesAGenerer; $numRessource++) {
-                $ressource = new Ressource();
-                $ressource -> setTitre($faker->sentence($nbWords = 6, $variableNbWords = true));
-                $ressource -> setDescriptif($faker->realText($maxNbChars = 200, $indexSize = 2));
-                $ressource -> setDateAjout($faker->dateTimeBetween($startDate = '-6 months', $endDate = 'now', $timezone = 'Europe/Paris'));
-                $ressource -> setUrlRessource($faker->url);
-                $ressource -> setUrlVignette($faker->imageUrl(400, 400, 'technics'));
-                // Création relation Ressource --> Module
-                $ressource -> addModule($module);
+	            // Définition du titre du stage
+	            $stage->setTitre($faker->sentence($nbWords = 5, $variableNbWords = true));
 
-                /****** Définir et mettre à jour le type de ressource ******/
-                // Sélectionner un type de ressource au hasard parmi les 8 types enregistrés dans $tableauFormation
-                $numTypeRessource = $faker->numberBetween($min = 0, $max = 7);
-                // Création relation Ressource --> TypeRessource
-                $ressource -> setTypeRessource($tableauFormation[$numTypeRessource]);
-                // Création relation TypeRessource --> Ressource
-                $tableauFormation[$numTypeRessource] -> addRessource($ressource);
+	            // Definition de sa description
+	            $stage->setDescription($faker->realText($maxNbChars = 200, $indexSize = 2));
 
-                // Persister les objets modifiés
-                $manager->persist($ressource);
-                $manager->persist($tableauFormation[$numTypeRessource]);
+	            // Definition de l'adresse mail
+	            $stage->setEmail($faker->companyEmail);
+
+	            // Enregistrement du stage
+	            $manager->persist($stage);
+
+	            // ************** Ajout de ce stage dans l'entreprise ********
+
+	            $entreprise->addStage($stage);
+	            $manager->persist($entreprise);
+
+	                /****** Mettre en lien le stage avec une ou plusieurs formation(s) ******/
+	                // On genere un nombre aleatoire de formations concernees par le stage
+	                $nbFormationsConcernees = $faker->numberBetween($min = 0, $max = 3);
+
+	                for ($m=0 ; $m <= $nbFormationsConcernees; $m++)
+	                {
+	                	// On choisit le numero de la formation concernee
+	                	$formationConcernee = $faker->numberBetween($min = 0, $max = 3);
+
+	                	$tableauFormations[$formationConcernee]->addStage($stage);
+
+	                	// On enregistre les modifications
+	                	$manager->persist($tableauFormations[$formationConcernee]);
+						
+	                }
+
+                $manager->flush();
             }
         }
-
-        $Formation = new Formation();
-        $Formation->setFormation("DUT Informatique");
-        $manager->persist($Formation);
-
-
-        $Entreprise = new Entreprise();
-        $Entreprise->setFormation($faker->company) 
-        $manager->flush();
+ 		
     }
 }
